4b73c8c25790aeb4183ba4e7d86bfc5b08266639
Patrick Snyder <117422938+Psnyder204@users.noreply.github.com>
2023-02-20 14:02:56 -0800

    Add files via upload

--- Score.js
+++ Score.js
@@ -2,13 +2,25 @@ class Score{
 
     constructor(){
         this.score = 0;
+
+      
+        
         
     }
 
-    display() {
+    displayPlayerScore() {
         fill(255);
         textSize(20);
-        text(`Score: ${this.score}`, 10, 20);
+        // textAlign(CENTER, TOP);
+        text(`Player: ${this.score}`, 200, 30);
+
+    }
+
+    displayAiScore() {
+        fill(255);
+        textSize(20);
+        // textAlign(CENTER, TOP);
+        text(`AI: ${this.score}`, 350, 30);
 
     }
 
